40544e293f0624bc4c3b19e22b3139c8692cadc1
jignesh <parijpatel1017@gmail.com>
2025-10-11 22:37:47 +0530

    Email by smtp

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+my_email = "parijpatel07@gmail.com"
+  # passs ="idbw dvsq herb vdyh"
+  # connection = smtplib.SMTP("smtp.gmail.com",587)
+  # connection.starttls()
+  # connection.login(user = my_email , password = passs)
+  # connection.sendmail(from_addr=my_email , to_addrs= "24cp035@bvmengineering.ac.in" , msg = "Ajj saturday hai padh leh nalayak padh leh")
+  # connection.close()
